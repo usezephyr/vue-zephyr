a9069f077d121b467d064ee326fd877b371bf345
Nate Dunn <hello@natedunn.net>
2021-01-05 15:44:37 -0600

    refactor: convert helpers to ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,7 @@
+import clsx from "clsx";
+import { tw } from "twind";
+
+// Remove
 export const remove = (list: string, remove: string): string => {
   if (!remove) return list;
   return list
@@ -6,4 +10,63 @@ export const remove = (list: string, remove: string): string => {
       return !remove.includes(c);
     })
     .join(" ");
+};
+
+// Classy
+export class Classy {
+  value: string | string[];
+
+  constructor(starter?: string | string[]) {
+    if (starter) {
+      this.value = Array.isArray(starter) ? starter : this.toArray(starter);
+    } else {
+      this.value = [];
+    }
+  }
+
+  // Chains
+  append(classes: string | string[]): this {
+    this.value = clsx(this.value, classes);
+    return this;
+  }
+
+  remove(classes: string | string[]): this {
+    const classArray =
+      typeof classes === "string" ? classes.split(" ") : classes;
+
+    this.value = this.toArray(this.value).filter(
+      className => !classArray.includes(className)
+    );
+    return this;
+  }
+
+  twind(enable = true): this {
+    if (enable) {
+      this.value = tw`${this.value}`;
+    }
+    return this;
+  }
+
+  val(type?: string): string | string[] {
+    if (type === "array") {
+      return this.toArray(this.value);
+    }
+    return clsx(this.value);
+  }
+
+  // Helpers
+  toArray(classes: string | string[]): string[] {
+    return typeof classes === "string" ? classes.split(" ") : classes;
+  }
+
+  toString(classes: string | string[]): string {
+    return Array.isArray(classes) ? classes.join(" ") : classes;
+  }
+}
+
+export const magicValues = (value: string): string => {
+  if (value.startsWith("$root")) {
+    return "";
+  }
+  return "";
 };
