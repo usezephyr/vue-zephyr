1659703fbff67a810e62bbf8e0704390e47ca953
Nate Dunn <hello@natedunn.net>
2021-01-16 21:49:01 -0600

    refactor: 💡 remove test variant

--- src/testTheme.ts
+++ src/testTheme.ts
@@ -3,7 +3,5 @@ export default {
     _base:
       "inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white leading-6 focus:ring-opacity-60 focus:outline-none focus:ring-2 focus:ring-offset-2",
     _default: "$root.danger",
-    danger:
-      "inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white leading-6 focus:ring-opacity-60 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:ring-offset-red-900 ring-offset-white bg-red-600 hover:bg-red-700 focus:ring-red-500"
-  }
+  },
 };
