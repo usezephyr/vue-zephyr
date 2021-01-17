8bed2c74017c37f7a0ff89793dab7f07344cac4f
Nate Dunn <hello@natedunn.net>
2021-01-16 21:22:14 -0600

    feat: 🎸 add postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: [
-    // require('tailwindcss'),
+    require('postcss-preset-env')({
+      features: {
+        'nesting-rules': true,
+      }
+    }),
     require('autoprefixer'),
     require('cssnano'),
   ]
