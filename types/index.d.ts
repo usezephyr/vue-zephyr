d653714dfa8db5e3be9e1def8151781079a6052e
Nate Dunn <hello@natedunn.net>
2021-01-19 15:50:46 -0600

    chore(types): add types

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,4 +5,8 @@ declare const vue3ComponentLibrary: Exclude<Plugin["install"], undefined>;
 export default vue3ComponentLibrary;
 
 export const Button: Exclude<Plugin["install"], undefined> | DefineComponent;
+export const Anchor: Exclude<Plugin["install"], undefined> | DefineComponent;
 export const Dropdown: Exclude<Plugin["install"], undefined> | DefineComponent;
+export const DropdownButton: Exclude<Plugin["install"], undefined> | DefineComponent;
+export const DropdownItems: Exclude<Plugin["install"], undefined> | DefineComponent;
+export const DropdownItem: Exclude<Plugin["install"], undefined> | DefineComponent;
